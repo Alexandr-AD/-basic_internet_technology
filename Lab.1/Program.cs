b5b43334c04094d4611203a86aaf082edc6d3ffd
Alexandr-AD <71642147+Alexandr-AD@users.noreply.github.com>
2020-11-02 16:59:28 +0300

    Add files via upload

---
+++ Lab.1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()//string[] args)
+        {
+       //system.console.setlocale(LC_ALL, "Russian");
+        //double a = 0, b = 0, c = 0, d = 0;
+        double a, b, c, d;
+
+            System.Console.WriteLine("Введите чилсло a",'\n');//endl);
+            a = Convert.ToDouble(System.Console.ReadLine());
+            System.Console.WriteLine("Введите чилсло b", '\n');
+            b = Convert.ToDouble(System.Console.ReadLine());
+            System.Console.WriteLine("Введите чилсло c" ,'\n');
+            c = Convert.ToDouble(System.Console.ReadLine());
+
+            if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                   System.Console.Write("Уравнение имеет бесконечное количество решений" ,'\n');
+                else
+                   System.Console.Write("Решений нет",'\n');
+
+            }
+            else
+               System.Console.Write("x = ", -c / b,'\n');
+        }
+        else
+        {
+
+            d = b * b - 4 * a * c;
+            if (d < 0)
+               System.Console.Write($"Решений нет {'\n'}");
+            else
+                if (d == 0)
+            {
+
+                System.Console.Write($"x = {-b / a} {'\n'}");
+            }
+            else
+            {
+                d = Math.Sqrt(d);
+                a *= 2;
+                    System.Console.WriteLine($"x1 = {(-b + d) / a}");
+                    System.Console.WriteLine($"x2 = {(-b - d) / a} { '\n'}");
+            }
+
+
+        }
+            Console.ReadLine();
+        //System("pause");
+
+       //return 0;
+    }
+}
+}
+
---
+++ Lab.1/ЛР№1 БКИТ.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()// argc, char [] argv)
+        {
+
+            double a, b, c, d;
+            System.Console.WriteLine("Выполнил: Алёшин Александр Денисович");
+            System.Console.WriteLine("Группа ИУ5-33Б\n");
+
+            System.Console.WriteLine("Введите чилсло a", '\n');//endl);
+            while (!double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Ошибка, повторите ввод");
+            }
+
+            System.Console.WriteLine("Введите чилсло b", '\n');
+            while (!double.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Ошибка, повторите ввод");
+            }
+            System.Console.WriteLine("Введите чилсло c", '\n');
+            while (!double.TryParse(Console.ReadLine(), out c))
+            {
+                Console.WriteLine("Ошибка, повторите ввод");
+            }
+
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    if (c == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        System.Console.Write("Уравнение имеет бесконечное количество решений", '\n');
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.Write("Решений нет", '\n');
+                        Console.ResetColor();
+                    }
+
+                }
+                else
+                        if ((-c / b) >= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    System.Console.Write("x1 = ", Math.Sqrt(-c / b), '\n');
+                    System.Console.Write("x2 = ", -Math.Sqrt(-c / b), '\n');
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.Write("Решений нет", '\n');
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+
+                d = b * b - 4 * a * c;
+                if (d < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.Write("Решений нет", '\n');
+                    Console.ResetColor();
+                }
+                else
+                    if (d == 0)
+                {
+                    if ((-b / 2 * a) >= 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        System.Console.Write($"x1 = {Math.Sqrt(-b / 2 * a)} {'\n'}");
+                        System.Console.Write($"x2 = {-Math.Sqrt(-b / 2 * a)} {'\n'}");
+                        Console.ResetColor();
+                    }
+                    else if ((-b / 2 * a) < 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.Write("Решений нет", '\n');
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    d = Math.Sqrt(d);
+                    a *= 2;
+                    double t = (-b + d) / a;
+                    if (t >= 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        System.Console.WriteLine($"x1 = {Math.Sqrt((-b + d) / a)}");
+                        System.Console.WriteLine($"x2 = {-Math.Sqrt((-b + d) / a)}");
+                    }
+                    t = (-b - d) / a;
+                    if (t >= 0)
+                    {
+                        System.Console.WriteLine($"x3 = {Math.Sqrt((-b - d) / a)}");
+                        System.Console.WriteLine($"x4 = {-Math.Sqrt((-b - d) / a)}");
+                        Console.ResetColor();
+                    }
+                    
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
