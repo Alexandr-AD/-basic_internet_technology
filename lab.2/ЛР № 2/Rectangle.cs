68c14a82edf29439bd665b44905cbcc917701fd4
Alexandr-AD <71642147+Alexandr-AD@users.noreply.github.com>
2020-12-26 17:48:09 +0300

    Add files via upload

---
+++ lab.2/ЛР № 2/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Rectangle : Figure, IPrint
+    {
+        /// <summary>
+        /// Высота
+        /// </summary>
+        public double height { get; set; }
+
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        public double width { get; set; }
+
+        /// <summary>
+        /// Основной конструктор
+        /// </summary>
+        /// <param name="ph">Высота</param>
+        /// <param name="pw">Ширина</param>
+        public Rectangle(double ph, double pw)
+        {
+            height = ph;
+            width = pw;
+            this.Type = "Прямоугольник";
+        }
+
+        /// <summary>
+        /// Вычисление площади
+        /// </summary>
+        public override double Area()
+        {
+            double Result = width * height;
+            return Result;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ lab.2/ЛР № 2/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Square : Rectangle, IPrint
+    {
+        public Square(double size)
+            : base(size, size)
+        {
+            this.Type = "Квадрат";
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public new void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
